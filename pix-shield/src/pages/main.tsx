a0b81f1fef0013c69f349578706c2064d4b1877f
Kleber Tadeu <klebertadeu17@gmail.com>
2025-11-16 22:46:06 -0300

    Inicio da configuração das rotas
    Inicio da configuração das rotas utilizando a React Router, com rotas configuradas em main.tsx. Inicialmente apenas com os path definidos das telas que temos disponiveis

--- pix-shield/src/pages/main.tsx
+++ pix-shield/src/pages/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Home from './Home'
 import './main.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Documentacao from './Documentacao'
 import Login from './Login'
 import Cadastro from './Cadastro'
-import Home from './Home'
-import Documentacao from './Documentacao'
+
+const router = createBrowserRouter([
+  {path: "/", element: <Home />},
+  {path: "/login", element: <Login />},
+  {path: "/cadastro", element: <Cadastro />},
+  // {path: "/perfil", element: }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Documentacao />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
