ed37e00cc4e026fda2112b586ab3a13eed9dc67d
Leonardo do Amaral Quinquio <leonarddoamaral@gmail.com>
2025-11-16 23:36:43 -0300

    Atualização card icones corretos e p arrumado
    Uma configuração do p  e do bi afetou outras telas acabei preferindo alterar.

--- pix-shield/src/pages/Documentacao/index.tsx
+++ pix-shield/src/pages/Documentacao/index.tsx
@@ -9,39 +9,39 @@ function Documentacao() {
             <Header />
             <div className='titulo-doc'>
                 <i className="bi bi-files"></i>
-                <h1>Documentação e Ajuda</h1>
+                <h1 className='h1-doc'>Documentação e Ajuda</h1>
             </div>
             
             <main className='main-doc'>
-                <h2>Sobre o PixShield</h2>
-                <p>Pix Shield é uma plataforma online e gratuita que funciona como central de reclamação de chaves pix com suspeitas de fraude. Nela o usuário pode pesquisar chaves pix, registrar denúncias e ter acesso a estatísticas sobre as mesmas.</p>
-                <h2>Como Usar a Plataforma</h2>
-                <p>Utilize a área de Pesquisa para verificar qualquer chave Pix antes de realizar uma transação.
+                <h2 className='subtitulo-doc'>Sobre o PixShield</h2>
+                <p className='p-doc'>Pix Shield é uma plataforma online e gratuita que funciona como central de reclamação de chaves pix com suspeitas de fraude. Nela o usuário pode pesquisar chaves pix, registrar denúncias e ter acesso a estatísticas sobre as mesmas.</p>
+                <h2 className='subtitulo-doc'>Como Usar a Plataforma</h2>
+                <p className='p-doc'>Utilize a área de Pesquisa para verificar qualquer chave Pix antes de realizar uma transação.
 
                     Ação: Digite a chave (e-mail, CPF/CNPJ, telefone ou chave aleatória) no campo de busca.
 
                     Resultado: O sistema informará se a chave possui denúncias registradas em nosso banco de dados.</p>
-                <h2>Fazer uma Denúncia</h2>
-                <p>Sua contribuição é vital para a segurança da comunidade.
+                <h2 className='subtitulo-doc'>Fazer uma Denúncia</h2>
+                <p className='p-doc'>Sua contribuição é vital para a segurança da comunidade.
 
                     Acesse a área Fazer Denúncia.
 
                     Preencha todos os campos obrigatórios, incluindo a chave Pix e uma descrição detalhada do ocorrido.Forneça o máximo de informações e provas</p>
-                <h2>Acompanhar Denúncia</h2>
-                <p>Após registrar uma denúncia, você pode acompanhar o processo de moderação.
+                <h2 className='subtitulo-doc'>Acompanhar Denúncia</h2>
+                <p className='p-doc'>Após registrar uma denúncia, você pode acompanhar o processo de moderação.
 
                     Acesse a área Acompanhar usando o ID fornecido no momento do registro.
 
                     Você verá o status da denúncia: Em Análise, Aprovada ou Rejeitada.</p>
-                <h2>Sobre a Moderação e Aprovação</h2>
-                <p>Toda denúncia é submetida a uma avaliação rigorosa pelo nosso time de administradores antes de ser integrada ao banco de dados.
+                <h2 className='subtitulo-doc'>Sobre a Moderação e Aprovação</h2>
+                <p className='p-doc'>Toda denúncia é submetida a uma avaliação rigorosa pelo nosso time de administradores antes de ser integrada ao banco de dados.
 
                     Aprovação: A denúncia será aprovada e a chave Pix será marcada como suspeita se as informações estiverem completas e a denúncia for considerada válida.
 
                     Rejeição: A denúncia será rejeitada se for identificada como incompleta, falsa ou, conforme nossa política, se o conteúdo for desrespeitoso, ofensivo, ou violar nossos termos de uso.</p>
 
-                <h2>Precisa de Ajuda?</h2>
-                <p>Se tiver dúvidas sobre o uso da plataforma ou encontrou um erro, entre em contato com nosso suporte técnico. Sua segurança é nossa prioridade.</p>
+                <h2 className='subtitulo-doc'>Precisa de Ajuda?</h2>
+                <p className='p-doc'>Se tiver dúvidas sobre o uso da plataforma ou encontrou um erro, entre em contato com nosso suporte técnico. Sua segurança é nossa prioridade.</p>
             </main>
         </>
     )
