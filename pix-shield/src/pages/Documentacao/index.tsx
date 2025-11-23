f2848d38286bb9070d193f93b4e89699bd20a3bc
Kleber Tadeu <klebertadeu17@gmail.com>
2025-11-23 20:16:27 -0300

    Polindo CSS - Página Perfil
    Código da página de Perfil foi revisado, implementando soluções necessárias para um layout responsivo e de fácil manutenção.

--- pix-shield/src/pages/Documentacao/index.tsx
+++ pix-shield/src/pages/Documentacao/index.tsx
@@ -1,3 +1,4 @@
+import Footer from "../../components/Footer";
 import Header from "../../components/Header"
 import './documentacao.css'
 
@@ -5,41 +6,37 @@ function Documentacao() {
     return (
         <>
             <Header />
-            <div className='titulo-doc'>
-                <h1 className='h1-doc'>Documentação e Ajuda</h1>
-            </div>
-            
-            <main className='main-doc'>
-                <h2 className='subtitulo-doc'>Sobre o PixShield</h2>
-                <p className='p-doc'>Pix Shield é uma plataforma online e gratuita que funciona como central de reclamação de chaves pix com suspeitas de fraude. Nela o usuário pode pesquisar chaves pix, registrar denúncias e ter acesso a estatísticas sobre as mesmas.</p>
-                <h2 className='subtitulo-doc'>Como Usar a Plataforma</h2>
-                <p className='p-doc'>Utilize a área de Pesquisa para verificar qualquer chave Pix antes de realizar uma transação.
 
-                    Ação: Digite a chave (e-mail, CPF/CNPJ, telefone ou chave aleatória) no campo de busca.
+            <section className="docContainer">
 
-                    Resultado: O sistema informará se a chave possui denúncias registradas em nosso banco de dados.</p>
-                <h2 className='subtitulo-doc'>Fazer uma Denúncia</h2>
-                <p className='p-doc'>Sua contribuição é vital para a segurança da comunidade.
+                <div className='titulo-doc'>
+                    <h1>Documentação e Ajuda</h1>
+                </div>
 
-                    Acesse a área Fazer Denúncia.
+                <main className='main-doc'>
+                    <h2>Sobre o PixShield</h2>
+                    <p>Pix Shield é a plataforma online e gratuita que redefine a segurança das transações, funcionando como a principal Central de Reclamação e Consulta de chaves Pix com suspeitas de fraude.
+                    Nascida como um projeto de inovação em ambiente universitário, a Pix Shield aplica inteligência e dados para capacitar o usuário</p>
 
-                    Preencha todos os campos obrigatórios, incluindo a chave Pix e uma descrição detalhada do ocorrido.Forneça o máximo de informações e provas</p>
-                <h2 className='subtitulo-doc'>Acompanhar Denúncia</h2>
-                <p className='p-doc'>Após registrar uma denúncia, você pode acompanhar o processo de moderação.
+                    <h2>Como Usar a Plataforma</h2>
+                    <p>Utilize a área de Pesquisa para verificar qualquer chave Pix antes de realizar uma transação.
 
-                    Acesse a área Acompanhar usando o ID fornecido no momento do registro.
+                        Ação: Digite a chave (e-mail, CPF/CNPJ, telefone ou chave aleatória) no campo de busca.
+                        Resultado: O sistema informará se a chave possui denúncias registradas em nosso banco de dados.</p>
 
-                    Você verá o status da denúncia: Em Análise, Aprovada ou Rejeitada.</p>
-                <h2 className='subtitulo-doc'>Sobre a Moderação e Aprovação</h2>
-                <p className='p-doc'>Toda denúncia é submetida a uma avaliação rigorosa pelo nosso time de administradores antes de ser integrada ao banco de dados.
+                    <h2>Fazer uma Denúncia</h2>
+                    <p>Sua contribuição é vital para a segurança da comunidade.
 
-                    Aprovação: A denúncia será aprovada e a chave Pix será marcada como suspeita se as informações estiverem completas e a denúncia for considerada válida.
+                        Acesse a área Fazer Denúncia.
+                        Preencha todos os campos obrigatórios, incluindo a chave Pix e uma descrição detalhada do ocorrido.Forneça o máximo de informações e provas</p>
+                        
+                    <h2>Precisa de Ajuda?</h2>
+                    <p>Se tiver dúvidas sobre o uso da plataforma ou encontrou um erro, entre em contato com nosso suporte técnico. Sua segurança é nossa prioridade.</p>
+                </main>
 
-                    Rejeição: A denúncia será rejeitada se for identificada como incompleta, falsa ou, conforme nossa política, se o conteúdo for desrespeitoso, ofensivo, ou violar nossos termos de uso.</p>
+            </section>
 
-                <h2 className='subtitulo-doc'>Precisa de Ajuda?</h2>
-                <p className='p-doc'>Se tiver dúvidas sobre o uso da plataforma ou encontrou um erro, entre em contato com nosso suporte técnico. Sua segurança é nossa prioridade.</p>
-            </main>
+            <Footer />
         </>
     )
 
--- pix-shield/src/pages/Perfil/index.tsx
+++ pix-shield/src/pages/Perfil/index.tsx
@@ -8,24 +8,24 @@ import { DenunciaAPI } from '../../api/denuncia';
 import Alerta from '../../components/alert/alert'
 import { jwtDecode } from 'jwt-decode';
 import type { JwtPayload } from 'jwt-decode';
-import CircularProgress  from '@mui/material/Alert'
+import CircularProgress from '@mui/material/Alert'
 
 interface CustomJwtPayload extends JwtPayload {
-    id_usuario: number; 
+    id_usuario: number;
     email_usuario: string;
 }
 
 function getUserIdFromToken(): number | null {
     const token = localStorage.getItem('authToken');
     if (!token) {
-        return null; 
+        return null;
     }
     try {
         const decoded = jwtDecode<CustomJwtPayload>(token);
-        return decoded.id_usuario; 
+        return decoded.id_usuario;
     } catch (e) {
         console.error("Erro ao decodificar o token:", e);
-        localStorage.removeItem('authToken'); 
+        localStorage.removeItem('authToken');
         return null;
     }
 }
@@ -43,7 +43,7 @@ function PerfilUser() {
     const [editMode, setEditMode] = useState(false);
     const [erro, setErro] = useState<string | null>(null);
     const [sucesso, setSucesso] = useState(false);
-   
+
 
     const toggleSenha = () => {
         setSenhaVisivel(!senhaVisivel);
@@ -120,114 +120,130 @@ function PerfilUser() {
     return (
         <>
             <Header />
+
+            {/* ----------------- ALERTAS ----------------- */}
             {editMode && <Alerta severity='info' messageAlert='Agora é permitido alterar os dados' />}
             {sucesso && <Alerta severity='success' messageAlert='Alteração realizada com sucesso!' />}
             {erro && <Alerta severity='error' messageAlert={erro} />}
-            <div className='containerPerfil'>
 
-                <aside className="infoGerais" aria-label="Informações do perfil">
-                    <section className="perfil">
-                        <figure className="avatarPerfil">
-                            <img
-                                src="./../src/assets/perfil.svg"
-                                alt="Foto de perfil"
-                                className="avatar-image"
-                            />
-                        </figure>
+            <main className='containerPerfil'>
 
-                        <div className="infoPerfil">
-                            <h2 className="profile-name">{nome}</h2>
-                            <p className="profile-email">{email}</p>
-                            <p className="profile-email">ID do usuário: {idUsuario}</p>
-                        </div>
 
+                <div className="bloco1">
+                    <aside className="infoGerais" aria-label="Informações do perfil">
+                        <section className="perfil">
+                            <figure className="avatarPerfil">
+                                <img
+                                    src="./../src/assets/perfil.svg"
+                                    alt="Foto de perfil"
+                                    className="avatar-image"
+                                />
+                            </figure>
+
+                            <div className="infoPerfil">
+                                <p>Nome: {nome}</p>
+                                <p>Email: {email}</p>
+                                <p>ID do usuário: {idUsuario}</p>
+                            </div>
+
+                        </section>
+                    </aside>
+                </div>
+
+                <div className="bloco2">
+                    <section className="infoPessoal">
+
+                        <h2>Informações Pessoais</h2>
+
+                        <form className="formInfoPessoal" onSubmit={handleSave}>
+                            <div className="areaNome">
+                                <label>Nome Completo</label>
+                                <input
+                                    type="text"
+                                    placeholder="Digite seu nome completo"
+                                    disabled={!editMode}
+                                    value={nome}
+                                    onChange={(e) => setNome(e.target.value)}
+                                />
+                            </div>
+                            <div className="areaEmail">
+                                <label>E-mail</label>
+                                <input
+                                    type="email"
+                                    placeholder="Digite seu e-mail"
+                                    disabled={!editMode}
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
+                            <div className="areaTelefone">
+                                <label>Telefone</label>
+                                <input
+                                    type="tel"
+                                    placeholder="Digite seu telefone"
+                                    disabled={!editMode}
+                                    value={telefone}
+                                    onChange={(e) => setTelefone(e.target.value)}
+                                />
+                            </div>
+                            <label>Senha</label>
+                            <div className="areaSenha">
+                                <input
+                                    type={inputType}
+                                    required
+                                    placeholder="Insira sua senha"
+                                    className='inputs'
+                                    disabled={!editMode}
+                                    value={senha}
+                                />
+                                <i className={iconClass} onClick={toggleSenha}></i>
+                            </div>
+                            <div className="botoes">
+
+                                <button
+                                    type="submit"
+                                    className="btnSalvar"
+                                    disabled={loading}
+                                >
+                                    {loading ? 'Salvando...' : 'Salvar Alterações'}
+                                </button>
+
+                                <button
+                                    type="button"
+                                    className="btnAlterar"
+                                    onClick={toggleEditMode}
+                                >
+                                    Alterar
+                                </button>
+
+                            </div>
+                        </form>
                     </section>
-                </aside>
+                </div>
 
-                <main className="infoPessoal">
-                    <h2>Informações Pessoais</h2>
-                    <form onSubmit={handleSave}>
-                        <div className="areaNome">
-                            <label>Nome Completo</label>
-                            <input
-                                type="text"
-                                placeholder="Digite seu nome completo"
-                                disabled={!editMode}
-                                value={nome}
-                                onChange={(e) => setNome(e.target.value)}
-                            />
-                        </div>
-                        <div className="areaEmail">
-                            <label>E-mail</label>
-                            <input
-                                type="email"
-                                placeholder="Digite seu e-mail"
-                                disabled={!editMode}
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        <div className="areaTelefone">
-                            <label>Telefone</label>
-                            <input
-                                type="tel"
-                                placeholder="Digite seu telefone"
-                                disabled={!editMode}
-                                value={telefone}
-                                onChange={(e) => setTelefone(e.target.value)}
-                            />
-                        </div>
-                        <label>Senha</label>
-                        <div className="areaSenha">
-                            <input
-                                type={inputType}
-                                required
-                                placeholder="Insira sua senha"
-                                className='inputs'
-                                disabled={!editMode}
-                                value={senha}
-                            />
-                            <i className={iconClass} onClick={toggleSenha}></i>
-                        </div>
-                        <div className="botoes">
+                <div className="bloco3">
+                    <section>
+                        <div className='denuncias'>
 
-                            <button
-                                type="submit"
-                                className="btnSalvar"
-                                disabled={loading}
-                            >
-                                {loading ? 'Salvando...' : 'Salvar Alterações'}
-                            </button>
+                            <h3 className='minha'>Minhas Denúncias Recentes</h3>
+                            <div className='cards-denuncias'>
+                                {denuncias.length > 0 ? (
+                                    denuncias.map((denuncia) => (
 
-                            <button
-                                type="button"
-                                className="btnAlterar"
-                                onClick={toggleEditMode}
-                            >
-                                Alterar
-                            </button>
-
+                                        <CardResults
+                                            key={denuncia.id_denuncia}
+                                            data={denuncia}
+                                        />
+                                    ))
+                                ) : (
+                                    <p>Você ainda não enviou denúncias.</p>
+                                )}
+                            </div>
                         </div>
-                    </form>
-                    <div className='denuncias'>
+                    </section>
+                </div>
 
-                        <h3 className='minha'>Minhas Denúncias Recentes</h3>
-                        <div className='cards-denuncias'>
-                            {denuncias.length > 0 ? (
-                                denuncias.map((denuncia) => (
-
-                                    <CardResults
-                                        key={denuncia.id_denuncia}
-                                        data={denuncia}
-                                    />
-                                ))
-                            ) : (
-                                <p>Você ainda não enviou denúncias.</p>
-                            )}
-                        </div>
-                    </div>
-                </main>
-            </div>
+            </main>
         </>
     )
 }
